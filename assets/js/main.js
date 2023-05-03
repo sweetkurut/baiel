771ddd625d0f867be2178fbe0b03c07b759a6905
sweetkurut <devxwc@gmail.com>
2023-05-03 13:08:02 +0600

    added slider-images

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,27 @@ document.addEventListener("click", (e) => {
     toggleMenu();
   }
 });
+
+// slider-images
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-fraction",
+    pagination: "fraction",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
